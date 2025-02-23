a1cf90b1a534eccbc9e4e789be8d018c56781bbf
Ayush Raj Saini <148912734+ayushrajsaini01@users.noreply.github.com>
2025-02-23 14:59:37 +0530

    DOne File Update
    DOne

---
+++ script.js
@@ -0,0 +1,439 @@
+const dishPrices = {
+
+     // Chicken Items
+     "Chicken Bhuna Half": 200,
+     "Chicken Bhuna Full": 400,
+     "Chicken Butter Masala Half": 200,
+     "Chicken Chicken Masala Full": 400,
+     "Chicken Kholapuri Half": 200,
+     "Chicken Kholapuri Full": 400,
+     "Chicken Masala Half": 200,
+     "Chicken Masala Full": 400,
+     "Chicken Hyderabad Half": 200,
+     "Chicken Hyderabad Full": 400,
+     "Chicken Kashmiri Half": 200,
+     "Chicken Kashmiri Full": 400,
+     "Chicken 2 Pyaza Half": 200,
+     "Chicken 2 Pyaza Full": 400,
+     "Chicken Chilli Bone": 120,
+     "Chicken Chilli Boneless Half": 150,
+     "Chicken Chilli Boneless Full": 300,
+     "Chicken 65": 240,
+     "Chicken Garlic": 240,
+     "Chicken Manchurian": 240,
+     "Chicken kadai Half": 200,
+     "Chicken kadai Full": 400,
+     "Chicken Handi Half": 200,
+     "Chicken Handi Full": 400,
+     "Egg Curry(2 Egg)": 90,
+     "Egg Bhurji Curry(2 Egg)": 100,
+
+     //Panneer Items
+
+     "Paneer Chilli Half": 150,
+     "Paneer Chilli Full": 300,
+     "Paneer 65": 220,
+     "Paneer Pokoda": 250,
+     "Panner Kofta": 250,
+     "Paneer Kadai Half": 150,
+     "Paneer Kadai Full": 300,
+     "Panner Butter Masala Half": 150,
+     "Panner Butter Masala Full": 300,
+     "Panner Handi Half": 150,
+     "Panner Handi Full": 300,
+     "Shahi Panner": 300,
+     "Panner Bhuna": 300,
+     "Panner Bhurji": 250,
+     "Mutter Panner Half": 150,
+     "Mutter Panner Full": 300,
+     "Palak Panner": 280,
+     "Panner 2 Pyaza Half": 150,
+     "Panner 2 Pyaza Full": 300,
+     "Panner Lajawab": 300,
+     
+     //Noodles
+     "Veg Noodles": 60,
+     "Hakka Noodles": 80,
+     "Sehezwan Noodles": 80,
+     "Egg Noodles Half": 50,
+     "Egg Noodles Full": 100,
+     "Chicken Noodles Half ": 60,
+     "Chicken Noodles Full ": 120,
+     "Schezwan Chicken Noodles Half ": 70,
+     "Schezwan Chicken Noodles Full ": 150,
+     "Schezwan Egg Noodles Half ": 60,
+     "Schezwan Egg Noodles Full ": 120,
+
+     //Tandoori
+     "Roti": 7,
+     "Tandoori Butter Roti": 10,
+     "Naan": 20,
+     "Butter Naan": 25,
+     "Garlic Naan": 30,
+     "Chicken Tandoori Half": 180,
+     "Chicken Tandoori Full": 360,
+     "Chicken Tikka": 150,
+     "Chicken Malai Tikka": 180,
+     "Panner Tikka": 150,
+     "Panner Malai Tikka": 180,
+
+     //Kulcha
+"Aloo": 50,
+"Aloo Cheese": 70,
+"Mix": 50,
+"Gobi": 50,
+"Panner": 70,
+
+//Roti N Paratha 
+"Tawa Roti": 6,
+"Tawa Roti with Butter": 8,
+"Sada Paratha": 10,
+"Aloo Paratha": 40,
+"Gobi Paratha": 40,
+"Panner Paratha": 50,
+"Chole Bhature": 50,
+
+//RASOI ITEMS
+"Dal Fry Half": 60,
+"Dal Fry Full": 120,
+"Dal Tadka Half": 80,
+"Dal Tadka Full": 150,
+"Mix Veg": 150,
+"Chana Masala": 120,
+"Mushroom Masala": 200,
+"Mushroom Curry": 200,
+"Mushroom Mattar": 200,
+"Mushroom Kadai": 200,
+"Mushroom Pyaza": 200,
+"Veg Kadai": 150,
+"Veg Kholapuri": 150,
+"Dal Makhani": 150,
+"Aloo Ghobi": 80,
+"Aloo Jeera": 80,
+"Aloo Palak": 80,
+"Aloo Mutter": 80,
+"Bhindi Masala": 100,
+
+//Rice
+"Steamed Rice": 60,
+"Jeera Rice": 70,
+"Fried Rice": 100,
+"Schezwan Rice": 120,
+"Manchurian Rice": 120,
+"Paneer Rice": 140,
+"Egg Rice Half": 80,
+"Egg Rice Full": 140,
+"Chicken Rice Half": 90,
+"Chicken Rice Full": 160,
+"Schezwan Egg Rice Half":100,
+"Schezwan Egg Rice Full":160,
+"Schezwan Chicken Rice Half":110,
+"Schezwan Chicken Rice Full":190,
+
+
+//Veg Roll / Frankey
+"Aloo Franky": 30,
+"Noodles Franky": 30,
+"Aloo Noodles Franky": 40,
+"Cheese Aloo Franky": 50,
+"Cheese Noodles Franky": 50,
+"Paneer Aloo Frankee": 50,
+"Paneer Noodles Franky": 50,
+"Mayo Aloo Franky": 50,
+
+
+//Non- Veg Roll
+"Single Egg Roll": 30,
+"Double Egg Roll": 40,
+"Chicken Roll": 60,
+"Bhurji (2 Egg)": 40,
+"Omlet (2 Egg)": 40,
+"Boiled Egg (2 Egg)": 20,
+
+//Raita
+"Dahi": 40,
+"Plain Raita": 30,
+"Mix Raita": 40,
+
+//Papad
+"Green Salad": 40,
+"Dry Papad": 20,
+"Fry Papad": 20,
+"Masala Papad": 30,
+
+//Beverages
+"Sweet Lassi": 30,
+"Hot Coffee": 20,
+"Mattha": 20,
+
+//Mutton Items
+"Mutton Curry Half": 150,
+"Mutton Curry Full": 300,
+"Mutton Keema Half": 150,
+"Mutton Keema Full": 300,
+"Mutton Kadai Half": 150,
+"Mutton Kadai Full": 300,
+"Mutton Kolapuri Half": 150,
+"Mutton Kolapuri Full": 300,
+"Mutton Butter Masala Half": 150,
+"Mutton Butter Masala Full": 300,
+"Mutton Bhuna Half": 150,
+"Mutton Bhuna Full": 300,
+
+// Roast
+"Chana Roast": 150,
+"Chicken Pakoda Half": 150,
+"Chicken Pakoda Full": 300,
+"Leg Roast": 100,
+"Chicken Roast Half": 150,
+"Chicken Roast Full": 300,
+"Lever Roast": 120,
+"Loly-Pop (4 Pcs)": 150,
+
+//Veg Chiili Item
+"Manchurian Half": 70,
+"Manchurian Full": 140,
+"Soya Chilli": 120,
+"Mushroom Chilli": 180,
+"Gobhi Chilli": 180,
+"Sweet Corn Chilli": 180,
+"Baby Corn Chilli": 200,
+
+//Biryani 
+"Veg Biryani": 120,
+"Paneer Biryani": 140,
+"Egg Biryani": 140,
+"Chicken Biryani Half": 100,
+"Chicken Biryani Full": 180,
+"Mutton Biryani Half": 120,
+"Mutton Biryani Full": 240,
+
+//Maggie 
+"Plain Maggie": 30,
+"Veg Maggie": 50,
+"Cheese Maggie": 80,
+"Paneer Maggie": 80,
+
+//Shakes
+"Cold Coffee": 50,
+"Cold Coffee With Ice-Cream": 70,
+"Oreo Shake": 60,
+"5 Star Shake": 60,
+"Kit-Kat Shake": 60,
+"Chocolate Shake": 50,
+"Strawberry Shake": 50,
+"Blueberry Shake": 50,
+"Valina Shake": 50,
+
+//Pizza
+"Classic Pizza": 100,
+"Classic Pizza": 120,
+"Sweetcorn Pizza": 120,
+"Paneer Tikka Pizza": 150,
+"Veg Overload Pizza": 150,
+"Farmhouse Pizza": 200,
+"Chicken Pizza": 200,
+"Chicken Farmhouse Pizza": 250,
+
+//French Fries
+"French Fries": 80,
+"Peri-Peri Fries": 100,
+"Cheese Loaded Fries": 120,
+
+};
+
+const dishes = Object.keys(dishPrices);
+
+function updatePrice(row) {
+    const quantity = row.querySelector(".quantity").value;
+    const dish = row.querySelector(".dish-input").value;
+    const price = dishPrices[dish] || 0;
+    const totalPrice = quantity * price;
+    row.querySelector(".price").textContent = price.toFixed(2);
+    row.querySelector(".totalPrice").textContent = totalPrice.toFixed(2);
+    updateOverallTotal();
+}
+
+function updateOverallTotal() {
+    const rows = document.querySelectorAll(".orderRow");
+    let overallTotal = 0;
+    rows.forEach(row => {
+        const totalPrice = parseFloat(row.querySelector(".totalPrice").textContent) || 0;
+        overallTotal += totalPrice;
+    });
+    document.querySelector(".overallTotal").textContent = overallTotal.toFixed(2);
+}
+
+function addRow() {
+    const table = document.querySelector("table tbody");
+    const newRow = document.createElement("tr");
+    newRow.className = "orderRow";
+    const serialNumber = table.querySelectorAll(".orderRow").length + 1;
+    newRow.innerHTML = `
+        <td><input type="text" class="serialNumber" size="5" value="${serialNumber}" readonly></td>
+        <td>
+            <div class="autocomplete">
+                <input type="text" class="dish-input" placeholder="Type to search..." />
+                <div class="autocomplete-items"></div>
+            </div>
+        </td>
+        <td><input type="number" class="quantity" value="0" min="0"></td>
+        <td class="price">0.00</td>
+        <td class="totalPrice">0.00</td>
+        <td><button onclick="deleteRow(this)">Delete</button></td>
+    `;
+    table.appendChild(newRow);
+    newRow.querySelector(".dish-input").addEventListener("input", function () {
+        autocomplete(this);
+    });
+    newRow.querySelector(".quantity").addEventListener("change", function () {
+        updatePrice(this.closest('tr'));
+    });
+}
+
+function deleteRow(button) {
+    const row = button.closest('tr');
+    row.parentNode.removeChild(row);
+    updateOverallTotal();
+}
+
+function autocomplete(input) {
+    const autocompleteItems = input.parentElement.querySelector(".autocomplete-items");
+    const val = input.value;
+    autocompleteItems.innerHTML = '';
+
+    if (!val) return false;
+
+    dishes.forEach(dish => {
+        if (dish.toLowerCase().includes(val.toLowerCase())) {
+            const item = document.createElement("div");
+            item.innerHTML = `<strong>${dish.substring(0, val.length)}</strong>${dish.substring(val.length)}`;
+            item.innerHTML += `<input type='hidden' value='${dish}'>`;
+            item.addEventListener("click", function () {
+                input.value = this.getElementsByTagName("input")[0].value;
+                updatePrice(input.closest('tr'));
+                autocompleteItems.innerHTML = '';
+            });
+            autocompleteItems.appendChild(item);
+        }
+    });
+}
+
+function generateOrderId() {
+    const today = new Date().toLocaleDateString();
+    let lastResetDate = localStorage.getItem('lastResetDate');
+
+    if (!lastResetDate || lastResetDate !== today) {
+        localStorage.setItem('orderId', '1');
+        localStorage.setItem('lastResetDate', today);
+        return 1;
+    } else {
+        let orderId = Number(localStorage.getItem('orderId'));
+        orderId++;
+        localStorage.setItem('orderId', orderId);
+        return orderId;
+    }
+}
+
+function printBill() {
+    const billContent = document.querySelector(".billContent");
+    const rows = document.querySelectorAll(".orderRow");
+    let billRows = '';
+
+    rows.forEach(row => {
+        const serialNumber = row.querySelector(".serialNumber").value;
+        const dish = row.querySelector(".dish-input").value;
+        const quantity = row.querySelector(".quantity").value;
+        const price = row.querySelector(".price").textContent;
+        const totalPrice = row.querySelector(".totalPrice").textContent;
+        if (dish) {
+            billRows += `
+                <tr>
+                    <td>${serialNumber}</td>
+                    <td>${dish}</td>
+                    <td>${quantity}</td>
+                    <td>${price}</td>
+                    <td>${totalPrice}</td>
+                </tr>
+            `;
+        }
+    });
+
+    billContent.innerHTML = `
+        <p id='orderID'>Order ID: ${generateOrderId()}</p>
+        <img src="canteen.png" alt="Restaurant Logo" width="150" height="auto" />
+        <p><strong>Bill Details:</strong></p>
+        <table style="width: 100%; font-size: 25px;">
+            <thead>
+                <tr>
+                    <th>Serial Number</th>
+                    <th>Dish</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${billRows}
+            </tbody>
+        </table>
+        <p class="total">Overall Total: Rs. <span class="overallTotal">${document.querySelector(".overallTotal").textContent}</span></p>
+        <button onclick="saveOrderDetails()">Save</button>
+    `;
+
+    const printWindow = window.open('', '', 'width=200,height=600');
+    printWindow.document.write('<html><head><title>Print Bill</title><style>body { font-size: 18px; margin: 0; } @media print { body { margin: 0; }</style></head><body>');
+    printWindow.document.write(`
+    <p style="font-weight: bold;">Date: ${new Date().toISOString().slice(0, 10)}</p>
+    <img src="canteen.png" alt="Restaurant Logo" width="150" height="auto" />
+    <p><strong>Bill Details:</strong></p>
+    <table style="width: 50%; font-size: 20px;">
+        <thead>
+            <tr>
+                <th>Sno.</th>
+                <th>Dish</th>
+                <th>Qnt</th>
+            </tr>
+        </thead>
+        <tbody>
+            ${billRows}
+        </tbody>
+    </table>
+    <p class="total">Overall Total: Rs. <span class="overallTotal">${document.querySelector(".overallTotal").textContent}</span></p>
+    <p id='orderID'>Order ID: ${generateOrderId()}</p>
+`);
+    printWindow.document.close();
+    printWindow.print();
+}
+
+function saveOrderDetails() {
+    const orderId = document.querySelector("#orderID").textContent.split(": ")[1];
+    const overallTotal = document.querySelector(".overallTotal").textContent;
+    const orderRows = Array.from(document.querySelectorAll(".orderRow")).map(row => {
+        const cells = row.querySelectorAll("td");
+        return [
+            cells[0].querySelector(".serialNumber").value,
+            cells[1].querySelector(".dish-input").value,
+            cells[2].querySelector(".quantity").value,
+            cells[3].textContent,
+            cells[4].textContent
+        ];
+    });
+
+    const doc = new jsPDF();
+    doc.setFontSize(20);
+    doc.text(`Order ID: ${orderId}`, 10, 10);
+    doc.text(`Date: ${new Date().toISOString().slice(0, 10)}`, 10, 20);
+    doc.setFontSize(14);
+    doc.text("Serial Number, Dish, Quantity, Price per Unit, Total Price", 10, 30);
+    orderRows.forEach((row, index) => {
+        doc.text(row.join(", "), 10, 40 + index * 10);
+    });
+    doc.text(`Overall Total: ${overallTotal}`, 10, 40 + orderRows.length * 10);
+
+    const now = new Date();
+    const timezoneOffsetInMilliseconds = now.getTimezoneOffset() * 60 * 1000;
+    const adjustedDate = new Date(now.getTime() - timezoneOffsetInMilliseconds);
+    const currentDate = adjustedDate.toISOString().slice(0, 10);
+    doc.save(`Order_${orderId}_${currentDate}.pdf`);
+}
